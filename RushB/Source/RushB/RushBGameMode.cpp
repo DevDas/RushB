6bee44bdc1b782fe69b5e677b32a1f83c4274a67
Dev Das <55701713+DevDas@users.noreply.github.com>
2021-05-10 13:38:35 +0530

    Added Assets

---
+++ RushB/Source/RushB/RushBGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RushBGameMode.h"
+#include "RushBCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ARushBGameMode::ARushBGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ RushB/Source/RushB/RushBGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RushBGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARushBGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARushBGameMode();
+};
+
+
+
